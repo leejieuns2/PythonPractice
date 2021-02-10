c21fc021cff66b8a9eefbfe93e64e3bb5d9d4785
leejieuns2 <2jjingni@gmail.com>
2021-02-11 02:40:59 +0900

    refactoring code

--- boj/1157.py
+++ boj/1157.py
@@ -9,11 +9,11 @@ str = str.upper()
 
 # 알파벳 개수 세기
 dic = {}
-for i in range(len(str)):
-    if str[i] in dic:
-        dic[str[i]] += 1
+for i in str:
+    if i in dic:
+        dic[i] += 1
     else:
-        dic[str[i]] = 1
+        dic[i] = 1
 
 # 가장 많이 사용된 알파벳 찾기
 max_key = max(dic, key=dic.get)
@@ -25,3 +25,29 @@ if max_value in dic.values():
     print("?")
 else:
     print(max_key)
+
+
+# 리팩토링 전
+# str = input()
+
+# # 문자열 전부 대문자로 변경 (대/소문자 구별 없이 같은 결과가 나와야 하므로)
+# str = str.upper()
+
+# # 알파벳 개수 세기
+# dic = {}
+# for i in range(len(str)):
+#     if str[i] in dic:
+#         dic[str[i]] += 1
+#     else:
+#         dic[str[i]] = 1
+
+# # 가장 많이 사용된 알파벳 찾기
+# max_key = max(dic, key=dic.get)
+# max_value = dic[max_key]
+# dic.pop(max_key)
+
+# # 가장 많이 사용된 알파벳이 여러 개일 경우 ? 출력
+# if max_value in dic.values():
+#     print("?")
+# else:
+#     print(max_key)
--- boj/2675.py
+++ boj/2675.py
@@ -2,25 +2,39 @@
 # 백준 2675. 문자열 반복
 # https://www.acmicpc.net/problem/2675
 
+
 t = int(input())
-
-# 테스트 케이스 개수가 정해져 있지 않으므로 각각 리스트에 담기
-rlist = []
-slist = []
-
 # 테스트 케이스 개수만큼 입력 받기
 for i in range(t):
-    r, s = input().split()
-    rlist.append(r)
-    slist.append(s)
-
-for i in range(t):
-    s = slist[i]
-    r = rlist[i]
-    rslt = ''
+    # split() 함수를 사용해 리스트로 입력값 받기
+    s = input().split()
 
     # 파이썬은 문자열 곱셈이 가능하므로 문자열 곱셈으로 새 string 만들기
-    for j in range(len(s)):
-        rslt = rslt + (s[j] * int(r))
+    for j in s[1]:
+        print(j * int(s[0]), end="")
+    print()
 
-    print(rslt)
+
+# 리팩토링 전
+    # t = int(input())
+
+    # # 테스트 케이스 개수가 정해져 있지 않으므로 각각 리스트에 담기
+    # rlist = []
+    # slist = []
+
+    # # 테스트 케이스 개수만큼 입력 받기
+    # for i in range(t):
+    #     r, s = input().split()
+    #     rlist.append(r)
+    #     slist.append(s)
+
+    # for i in range(t):
+    #     s = slist[i]
+    #     r = rlist[i]
+    #     rslt = ''
+
+    #     # 파이썬은 문자열 곱셈이 가능하므로 문자열 곱셈으로 새 string 만들기
+    #     for j in range(len(s)):
+    #         rslt = rslt + (s[j] * int(r))
+
+    #     print(rslt)
